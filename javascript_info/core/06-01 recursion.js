d7fa07a676fad5625eb80410fff34f5bb9aa4677
hwanghyekyung <hwanghyekyung1004@gmail.com>
2021-03-19 23:53:22 +0900

    문제풀이

--- javascript_info/core/06-01 recursion.js
+++ javascript_info/core/06-01 recursion.js
@@ -197,19 +197,6 @@ function fib(n){
 
 // 그리고 재귀를 사용한 것과 재귀를 사용하지 않은 것 중 어떤 게 더 좋은 코드인지 생각해봅시다.
 
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null
-      }
-    }
-  }
-};
 
 function printList(list){
     // let ref = new Array(list);
@@ -259,45 +246,37 @@ function printList(list){
     
 }
 
-printList(list);
+//printList(list);
 
 // console.log(ref);
  
 
 
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+ 
 
-
-
-
-
-// 단일 연결 리스트 출력하기
-// 중요도: 5
-// 재귀와 스택에서 설명한 바 있는, 단일 연결 리스트(single-linked list)가 있다고 가정해 봅시다.
-
-// let list = {
-//   value: 1,
-//   next: {
-//     value: 2,
-//     next: {
-//       value: 3,
-//       next: {
-//         value: 4,
-//         next: null
-//       }
-//     }
-//   }
-// };
-// 리스트 내 항목을 차례대로 하나씩 출력해주는 함수 printList(list)를 만들어보세요.
-
-// 반복문과 재귀를 사용한 답안을 각각 만들어봅시다.
-
-// 그리고 재귀를 사용한 것과 재귀를 사용하지 않은 것 중 어떤 게 더 좋은 코드인지 생각해봅시다.
-
-// 해답
 // 단일 연결 리스트를 역순으로 출력하기
 // 중요도: 5
 // 위 문제(단일 연결 리스트 출력하기)에 있는 단일 연결 리스트의 항목을 역순으로 출력해주는 함수를 만들어봅시다.
 
 // 반복문과 재귀를 사용한 답안을 각각 만들어보세요.
 
-// 해답
+
+function printReverse(list){
+ // 반복문 
+ console.log(Object.keys(list).length+1);
+
+}
+printReverse(list);

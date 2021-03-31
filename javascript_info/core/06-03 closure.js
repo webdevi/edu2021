6dfb229ed7d7c14fb2ba6d39197ffa07f6f37ce8
hwanghyekyung <hwanghyekyung1004@gmail.com>
2021-03-31 23:51:03 +0900

    클로저문제

--- javascript_info/core/06-03 closure.js
+++ javascript_info/core/06-03 closure.js
@@ -1,4 +1,4 @@
-//"use strict";
+"use strict";
 
 // 코드 블록 
 // : 코드 블록{...} 안에 선언한 변수는 블록 안에서만 사용할 수 있다 .
@@ -178,4 +178,60 @@ function sum(a){
 }
 
 sum(1)(2);
+
+
+console.log(`...... Is variable visible?.........`);
+
+// Is variable visible?
+// 중요도: 4
+// What will be the result of this code?
+
+let x = 1; 
+function func() {
+   console.log(x); // ?
+
+  //  let x = 2;
+}
+func();
+// P.S. There’s a pitfall in this task. The solution is not obvious.
+
+
+console.log(`...... 함수를 이용해 원하는 값만 걸러내기 .........`);
+
+// 중요도: 5
+// 배열에 사용할 수 있는 내장 메서드 arr.filter(f)는 함수 f의 반환 값을 true로 만드는 모든 요소를 배열로 반환해줍니다.
+
+// filter에 넘겨서 사용할 수 있는 함수 두 가지를 만들어봅시다.
+
+// inBetween(a, b) – a 이상 b 이하
+// inArray([...]) – 배열 안에 있는 값인가
+// 위 함수를 활용하면 다음과 같은 결과가 나와야 합니다.
+
+// arr.filter(inBetween(3,6)) – 3과 6 사이에 있는 값만 반환함
+// arr.filter(inArray([1,2,3])) – [1,2,3] 안에 있는 값과 일치하는 값만 반환함
+// 예시:
+
+// /* ... 여기에 두 함수 inBetween과 inArray을 만들어주세요 ...*/
+let arr = [1, 2, 3, 4, 5, 6, 7];
+// inBetween(3, 6);
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6 
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+function inBetween(a, b){
+    return function(x){
+      return x >= a  && x <=b;
+    };
+} 
+
+function inArray(arr){
+  return function(x){
+    return arr.includes(x);
+  };
+}
+
+
+// function isArray(arr){
+
+// }
+
 

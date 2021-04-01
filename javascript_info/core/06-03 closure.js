885e7e92c9be365308043ab72a054e365b5e0fb0
hwanghyekyung <hwanghyekyung1004@gmail.com>
2021-04-01 23:08:17 +0900

    클로저 문제

--- javascript_info/core/06-03 closure.js
+++ javascript_info/core/06-03 closure.js
@@ -214,8 +214,8 @@ console.log(`...... 함수를 이용해 원하는 값만 걸러내기 .........`
 // /* ... 여기에 두 함수 inBetween과 inArray을 만들어주세요 ...*/
 let arr = [1, 2, 3, 4, 5, 6, 7];
 // inBetween(3, 6);
-alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6 
-alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+console.log( arr.filter(inBetween(3, 6)) ); // 3,4,5,6 
+console.log( arr.filter(inArray([1, 2, 10])) ); // 1,2
 
 function inBetween(a, b){
     return function(x){
@@ -229,9 +229,44 @@ function inArray(arr){
   };
 }
 
+console.log(`...... 필드를 기준으로 정렬하기.........`);
 
-// function isArray(arr){
+// 중요도: 5
+// 객체가 담긴 배열을 정렬해야 한다고 가정해봅시다.
 
-// }
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+// users.sort((a,b) => a.name > b.name ? 1 : -1 );
+
+// 아래와 같은 방법을 사용해 정렬할 수 있을 겁니다.
+
+// // 이름을 기준으로 정렬(Ann, John, Pete)
+// users.sort((a, b) => a.name > b.name ? 1 : -1);
+
+// // 나이를 기준으로 정렬(Pete, Ann, John)
+// users.sort((a, b) => a.age > b.age ? 1 : -1);
+// 그런데, 아래와 같이 함수를 하나 만들어서 정렬하면 더 깔끔해질 것 같네요.
+
+// users.sort(byField('name'));
+// users.sort(byField('age'));
+// 함수를 직접 만들어 sort에 넘기는 것보다 byField(fieldName)를 넘기는 것처럼 말이죠.
+
+// 필드를 기준으로 정렬을 도와주는 함수 byField를 만들어봅시다.
+
+function byField(param){
+  return function(a, b){
+    return a[param] > b[param] ? 1 : -1;
+  };
+}
+
+// users.sort(byField('name'));
+// console.log(users);
+ users.sort(byField('age'));
+ console.log(users);
 
 
+ 

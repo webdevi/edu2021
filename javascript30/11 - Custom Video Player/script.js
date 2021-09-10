a38fd75582485392f4d31ac5ccb5e7b2742dd7d5
hwanghyekyung <hwanghyekyung1004@gmail.com>
2021-09-10 23:59:05 +0900

    완성

--- javascript30/11 - Custom Video Player/script.js
+++ javascript30/11 - Custom Video Player/script.js
@@ -7,6 +7,7 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+
 // Build out functions
 function toggleVideo(){
   const methodnm = video.paused? "play" : "pause"; 
@@ -17,10 +18,44 @@ function toggleIcon(){
   toggle.textContent = video.paused? "▶" : "| |";  
 }
 
+function volumnControl(){ 
+  video.volumn = parseFloat(this.value);
+  console.log(video.volumn);
+}
+
+function speedContorl(){
+  console.log(`speed : ${this.value}`);  
+  video.playbackrate += parseFloat(this.value);
+} 
+function handlerangeUpdate(){
+  video[this.name]  = this.value;
+}
+function skip(){
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip); 
+} 
+function handelProgress(){
+  const percent = (video.currentTime / video.duration ) * 100; 
+  progressBar.style.flexBasis = `${percent}%`; 
+} 
+function playskip(e){  
+  const playpoint = (e.offsetX / progress.offsetWidth) * video.duration ;
+  video.currentTime = playpoint;
+}
 // Hook up the event listners
 video.addEventListener('click', toggleVideo);
 toggle.addEventListener('click', toggleVideo);
 
-video.addEventListener('click', toggleIcon);
+video.addEventListener('play', toggleIcon);
+video.addEventListener('timeupdate', handelProgress);
+video.addEventListener('pause', toggleIcon);
+
 toggle.addEventListener('click', toggleIcon);
+
+skipButtons.forEach(skipButton=>skipButton.addEventListener('click', skip));
+ranges.forEach(range=>range.addEventListener('change', handlerangeUpdate));
+/*mousemove 이벤트를 태워야 조금 더 자연스럽게 작동을 함 */
+ranges.forEach(range=>range.addEventListener('mousemove', handlerangeUpdate));
+progress.addEventListener("click", playskip );
+progress.addEventListener("mousemove", playskip );
 
